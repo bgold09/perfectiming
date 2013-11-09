20e8bc268728c6c66a9c9f4744a89edced9f9ee7
BRIAN J GOLDEN <bwg5079@waring107-lab-mac152.clc-labs.its.psu.edu>
2013-11-08 19:32:02 -0500

    add table view controller subclasses for app features; add slide out menu

---
+++ PerfecTiming/PTSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  PTSettingsViewController.m
+//  PerfecTiming
+//
+//  Created by BRIAN J GOLDEN on 11/8/13.
+//  Copyright (c) 2013 BRIAN J GOLDEN. All rights reserved.
+//
+
+#import "PTSettingsViewController.h"
+
+@interface PTSettingsViewController ()
+
+@end
+
+@implementation PTSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
