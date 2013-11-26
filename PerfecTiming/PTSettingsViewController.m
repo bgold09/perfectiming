b252c79c1c2239cff2f4c27ff4ee6089b7b97e7e
Brian Golden <brian.golden09@gmail.com>
2013-11-26 00:52:37 -0500

    change layout of settings view

--- PerfecTiming/PTSettingsViewController.m
+++ PerfecTiming/PTSettingsViewController.m
@@ -12,8 +12,14 @@
 #import "Constants.h"
 
 @interface PTSettingsViewController ()
+
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *menuButton;
+@property (weak, nonatomic) IBOutlet UITableViewCell *usernameCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *emailCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *nameCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *logoutCell;
 - (IBAction)logoutPressed:(id)sender;
+- (IBAction)editPressed:(id)sender;
 
 @end
 
@@ -22,7 +28,7 @@
 - (id)initWithStyle:(UITableViewStyle)style {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
@@ -32,12 +38,26 @@
     self.menuButton.target = self.revealViewController;
     self.menuButton.action = @selector(revealToggle:);
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    self.usernameCell.detailTextLabel.text = [[PFUser currentUser] username];
+    self.emailCell.detailTextLabel.text = [[PFUser currentUser] email];
+    self.nameCell.detailTextLabel.text = [[PFUser currentUser] objectForKey:@"name"];
+    
+    UIView *backView = [[UIView alloc] initWithFrame:CGRectZero];
+    backView.backgroundColor = [UIColor clearColor];
+    self.logoutCell.backgroundView = backView;
 }
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
+}
+
+#pragma mark - Editing
+
+- (IBAction)editPressed:(id)sender {
+    
 }
 
 #pragma mark - Logout User
